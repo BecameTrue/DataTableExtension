59dcbd9bdeaf3ba083a78b9efac24a5c5406edf4
we know <csh_do@naver.com>
2020-07-29 20:51:41 +0900

    testing

--- DataTableExtension.js
+++ DataTableExtension.js
@@ -19,7 +19,7 @@
   var configure = () => {
     // 다이얼로그 HTML 파일 경로
     const dialogURL =
-      "https://chash.in/DataTableExtension/DataTableExtension.html";
+      "https://chash.in/DataTableExtension/ConfigureDialog.html";
 
     /* 
       다이얼로그에게 전달해주고 싶은 값(문자열만 가능!)
